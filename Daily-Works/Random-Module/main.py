d988a5bca251b7d06ce96d43860104c46e098c2f
Arnab <arnabchy424@gmail.com>
2026-05-10 19:41:56 +0545

    Update main.py

--- Daily-Works/Random-Module/main.py
+++ Daily-Works/Random-Module/main.py
@@ -1,47 +1,47 @@
-# # Work 1 :  Generate 6 random numbers between 1 and 50 for a lottery simulation.
-# from random import randint
-# for i in range(1,7):
-#     print(randint(1,50))
+# Work 1 :  Generate 6 random numbers between 1 and 50 for a lottery simulation.
+from random import randint
+for i in range(1,7):
+    print(randint(1,50))
 
-# # Work 2 :  Create a random password using letters and digits for account security.
-# from random import choice
+# Work 2 :  Create a random password using letters and digits for account security.
+from random import choice
 
-# key = [
-#     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
-#     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-#     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
-#     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-#     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-# ]
+key = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+]
 
-# len = int(input("Enter the len of password : "))
+len = int(input("Enter the len of password : "))
 
-# for i in range(len+1):
-#     passwd = choice(key)
-#     print(passwd, end = "" )
-# print('\n')
+for i in range(len+1):
+    passwd = choice(key)
+    print(passwd, end = "" )
+print('\n')
 
 
-# # Work 3 :  Randomly select a question from a list of practice questions to quiz yourself.
-# # Using Choice func again
+# Work 3 :  Randomly select a question from a list of practice questions to quiz yourself.
+# Using Choice func again
 
-# questions = [
-#     ["What is your name?"],
-#     ["How old are you?"],
-#     ["Where do you live?"],
-#     ["What is your favorite color?"],
-#     ["What do you you do for work?"],
-#     ["What are your hobbies?"],
-#     ["What is your favorite food?"],
-#     ["What languages do you speak?"],
-#     ["What is your biggest goal?"],
-#     ["What makes you happy?"]
-# ]
+questions = [
+    ["What is your name?"],
+    ["How old are you?"],
+    ["Where do you live?"],
+    ["What is your favorite color?"],
+    ["What do you you do for work?"],
+    ["What are your hobbies?"],
+    ["What is your favorite food?"],
+    ["What languages do you speak?"],
+    ["What is your biggest goal?"],
+    ["What makes you happy?"]
+]
 
-# for question in questions:
-#     random_question = choice(question)
+for question in questions:
+    random_question = choice(question)
 
-# print(random_question)
+print(random_question)
 
 
 # Work 4 : 
